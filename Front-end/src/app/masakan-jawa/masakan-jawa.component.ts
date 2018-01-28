329efbc62fd9e097c3d8c707e7c83c33c8c9524a
bahtera <bimabahtera@gmail.com>
2018-01-28 15:11:23 +0700

    first commit

---
+++ Front-end/src/app/masakan-jawa/masakan-jawa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-masakan-jawa',
+  templateUrl: './masakan-jawa.component.html',
+  styleUrls: ['./masakan-jawa.component.css']
+})
+export class MasakanJawaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
